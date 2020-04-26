5d562991f564fae9522cc86b32270d6008516e35
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-04-26 08:54:13 +0430

    Cleanup the code

--- src/main/java/com/javastudio/tutorial/security/JwtRequestFilter.java
+++ src/main/java/com/javastudio/tutorial/security/JwtRequestFilter.java
@@ -24,7 +24,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     private JwtUserDetailsService jwtUserDetailsService;
 
     @Autowired
-
     private JwtTokenUtil jwtTokenUtil;
 
     @Override
